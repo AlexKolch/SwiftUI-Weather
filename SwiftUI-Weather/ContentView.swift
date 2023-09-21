f28e90780b3fdb17c7e58015af60c301246d52cb
Alexey Kolch <kolychenkov94@mail.ru>
2023-09-21 19:27:07 +0400

    week add

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(colors: [.blue, Color("myCyan")]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
             VStack {
                 Text("Cupertino, CA")
@@ -27,11 +27,14 @@ struct ContentView: View {
                     Text("76°")
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
-                }
-                HStack {
-                    VStack {
-                        
-                    }
+                }.padding(.bottom, 40)
+
+                HStack(spacing: 20.0) {
+                    WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 74)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
+                    WeatherDayView(dayOfWeek: "THU", imageName: "wind.snow", temperature: 60)
+                    WeatherDayView(dayOfWeek: "FRY", imageName: "cloud.sun.fill", temperature: 76)
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "sunset.fill", temperature: 68)
                 }
                 Spacer()
             }
@@ -42,5 +45,25 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+
+    var body: some View {
+        VStack {
+            Text(dayOfWeek).foregroundColor(.white).font(.system(size: 16, weight: .medium))
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
     }
 }
